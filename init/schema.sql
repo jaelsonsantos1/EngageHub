53f1e83af9f2969b20d051e7e0c4392658106e6e
jaelsonsantos1 <jaelsons860@gmail.com>
2024-03-25 15:25:32 -0300

    :recycle: config: Update in sql scheme

--- init/schema.sql
+++ init/schema.sql
@@ -1,67 +1,67 @@
 CREATE DATABASE IF NOT EXISTS wva_database;
 
-CREATE TABLE IF NOT EXISTS wva_database.leaders (
+CREATE TABLE IF NOT EXISTS leaders (
   id SERIAL,
   full_name VARCHAR(50) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   actived BOOLEAN DEFAULT TRUE,
 
   PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS wva_database.sectors (
+CREATE TABLE IF NOT EXISTS sectors (
     id SERIAL,
     full_name VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     actived BOOLEAN DEFAULT TRUE,
 
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS wva_database.collaborators (
+CREATE TABLE IF NOT EXISTS collaborators (
     id SERIAL,
-    id_leader VARCHAR(150) NOT NULL,
-    id_sector VARCHAR(150) NOT NULL,
+    id_leader SERIAL NOT NULL,
+    id_sector SERIAL NOT NULL,
     full_name VARCHAR(50) NOT NULL,
     age INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     actived BOOLEAN DEFAULT TRUE,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id_leader) REFERENCES wva_database.leaders(id),
-    FOREIGN KEY (id_sector) REFERENCES wva_database.sectors(id)
+    FOREIGN KEY (id_leader) REFERENCES leaders(id),
+    FOREIGN KEY (id_sector) REFERENCES sectors(id)
 );
 
-CREATE TABLE IF NOT EXISTS wva_database.productivity (
+CREATE TABLE IF NOT EXISTS productivity (
     id SERIAL,
-    id_collaborator VARCHAR(150) NOT NULL,
-    id_leader VARCHAR(150) NOT NULL,
+    id_colaborator SERIAL NOT NULL,
+    id_leader SERIAL NOT NULL,
     evaluation_note INTEGER NOT NULL,
     comment TEXT,
     goal VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     actived BOOLEAN DEFAULT TRUE,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id_collaborator) REFERENCES wva_database.collaborators(id),
-    FOREIGN KEY (id_leader) REFERENCES wva_database.leaders(id)
+    FOREIGN KEY (id_colaborator) REFERENCES collaborators(id),
+    FOREIGN KEY (id_leader) REFERENCES leaders(id)
 );
 
-CREATE TABLE IF NOT EXISTS wva_database.engagement (
+CREATE TABLE IF NOT EXISTS engagement (
     id SERIAL,
-    id_collaborator VARCHAR(150) NOT NULL,
-    id_leader VARCHAR(150) NOT NULL,
+    id_colaborator SERIAL NOT NULL,
+    id_leader SERIAL NOT NULL,
     goal INTEGER NOT NULL,
     comment TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     actived BOOLEAN DEFAULT TRUE,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id_collaborator) REFERENCES wva_database.collaborators(id),
-    FOREIGN KEY (id_leader) REFERENCES wva_database.leaders(id)
+    FOREIGN KEY (id_colaborator) REFERENCES collaborators(id),
+    FOREIGN KEY (id_leader) REFERENCES leaders(id)
 );
